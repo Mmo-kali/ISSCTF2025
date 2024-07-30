8f63e1071b2965520dae058214234aedd9efc4f3
Prajwal Nautiyal <117471245+PrajwalNa@users.noreply.github.com>
2024-07-30 20:57:54 +0000

    Add files via upload

---
+++ 02-REVERSING/Piece of The PiE/operations.h
@@ -0,0 +1,12 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+class Operations {
+public:
+    static int add(int a, int b);
+    static int subtract(int a, int b);
+    static int multiply(int a, int b);
+    static int divide(int a, int b);
+    };
+
+#endif // OPERATIONS_H
