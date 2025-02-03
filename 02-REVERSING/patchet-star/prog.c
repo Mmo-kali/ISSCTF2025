fcaae808533b2b200cf19a42f0b1486824730004
mm0 <162916276+Mmo-kali@users.noreply.github.com>
2025-02-03 10:06:18 -0500

    Update prog.c

--- 02-REVERSING/patchet-star/prog.c
+++ 02-REVERSING/patchet-star/prog.c
@@ -3,13 +3,25 @@
 #include <unistd.h>
 #include <sys/ptrace.h>
 #include <ctype.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 
 void anti_debug() {
     if (ptrace(PTRACE_TRACEME, 0, 1, 0) == -1) {
         printf("Debugger detected!\n");
         exit(1);
     }
+}
+
+void get_flag(char *output) {
+    unsigned char obfuscated_flag[] = {
+        0x3d, 0xf1, 0x40, 0x6a, 0x28, 0x69, 0xf0, 0x7e, 0x20, 0x68, 0x5b, 0x56,
+        0x36, 0x70, 0x2b, 0x60
+    };
+    char key = 0x4F; // XOR key
+    for (int i = 0; i < sizeof(obfuscated_flag); i++) {
+        output[i] = obfuscated_flag[i] ^ key;
+    }
+    output[sizeof(obfuscated_flag)] = '\0'; // null-terminate the string
 }
 
 int check_key_stage1(const char *key) {
@@ -41,11 +53,12 @@ int main() {
     scanf("%255s", key);
 
     if (check_key(key)) {
-        printf("Correct key! The flag is shadowCTF\n");
+        char flag[50];
+        get_flag(flag);
+        printf("Correct key! The flag is: %s\n", flag);
     } else {
         printf("Incorrect key.\n");
     }
 
     return 0;
 }
-
